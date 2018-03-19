debcc0229c8122b56189e7aadf6800d6be467de6
Nickolas981 <kolin98111@gmail.com>
2018-03-19 15:19:30 +0200

    Created Date Util

---
+++ app/src/main/java/com/example/ngumeniuk/curogram/utils/DateUtil.kt
@@ -0,0 +1,18 @@
+package com.example.ngumeniuk.curogram.utils
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by ngumeniuk on 3/19/18.
+ */
+class DateUtil {
+    companion object {
+        @SuppressLint("SimpleDateFormat")
+        fun formatedDate(): String =
+                SimpleDateFormat("yyyy/MM/dd/kk:mm:ss")
+                        .format(Calendar.getInstance().time)
+
+    }
+}

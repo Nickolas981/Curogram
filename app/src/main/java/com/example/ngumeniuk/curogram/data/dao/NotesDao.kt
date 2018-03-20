35bc06d26ff707c74360f9da2f0d2c579cbf0293
Nickolas981 <kolin98111@gmail.com>
2018-03-20 12:30:57 +0200

    Changed notes dao

--- app/src/main/java/com/example/ngumeniuk/curogram/data/dao/NotesDao.kt
+++ app/src/main/java/com/example/ngumeniuk/curogram/data/dao/NotesDao.kt
@@ -2,6 +2,7 @@ package com.example.ngumeniuk.curogram.data.dao
 
 import android.arch.persistence.room.*
 import com.example.ngumeniuk.curogram.data.models.NoteModel
+import io.reactivex.Flowable
 import io.reactivex.Single
 
 
@@ -13,12 +14,15 @@ interface NotesDao {
     @Delete
     fun delete(note: NoteModel)
 
-    @Query("SELECT * FROM notes")
-    fun getAll(): Single<NoteModel>
+    @Query("SELECT * FROM notes ORDER BY date DESC")
+    fun getAll(): Flowable<List<NoteModel>>
 
     @Query("SELECT * FROM notes WHERE id LIKE :id")
-    fun selectById(id: Int)
+    fun selectById(id: Int): Single<NoteModel>
 
     @Update
     fun updateNote(note: NoteModel)
+
+    @Query("DELETE FROM notes")
+    fun dropTable()
 }

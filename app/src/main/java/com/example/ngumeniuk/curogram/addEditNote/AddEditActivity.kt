bb4ae37b9b0b3ef3e11df80ccdc843b58bf2d258
Nickolas981 <kolin98111@gmail.com>
2018-03-20 12:37:34 +0200

    Created AddEditActivity

---
+++ app/src/main/java/com/example/ngumeniuk/curogram/addEditNote/AddEditActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ngumeniuk.curogram.addEditNote
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.ngumeniuk.curogram.R
+
+class AddEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit)
+    }
+}
